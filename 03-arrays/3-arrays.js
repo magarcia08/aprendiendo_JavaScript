7449dc93e2e13e399a6a553dfbe9fb53d93696fd
magarcia08 <mariagarcia36508@gmail.com>
2025-11-28 19:06:47 -0500

    feat: :sparkles: agregando mas informacion

--- 03-arrays/3-arrays.js
+++ 03-arrays/3-arrays.js
@@ -5,3 +5,50 @@ const mixtos = ["papas", "patacones", "chicarrones", "arepas"];
 
 
 
+const frutasExoticas = new Array ("kiwi", "chontaduro", "macadami", "granada");
+
+
+// declaracion con array.from()
+const letras = Array.from("hola");
+
+
+// declaracion con array of
+const letras2 = Array.of(7)
+
+const numeros = Array.of(7)
+const numeros2 = Array.of(5,10,20)
+
+// recorrido de los arrays por los elementos
+
+for (let fe of frutasExoticas) {
+    console.log(fe);
+}
+
+
+// metodos de los arrays
+
+// metodo push : agrega al final
+
+ numeros.push(13);
+ console.log(numeros);
+
+ // metodo unshift : metodo agregar al inicio
+ numeros.unshift(80);
+console.log(numeros);
+
+// metodo pop :  este metodo elimina el ultimo elemento 
+numeros.pop();
+console.log(numeros);
+
+// metodo shift : elimina al primer elemento
+numeros.shift();
+console.log(numeros);
+
+// metodos funcionales
+
+// metodo map
+
+//recorre el array y crea uno nuevo aplicandole una funcion a cada elemento
+
+console.log(frutasExoticas.map(fruta => fruta.toUpperCase()));
+
---
+++ README.md
@@ -0,0 +1,10 @@
+## Aprendiendo JavaScript
+
+
+# Este repositorio encontrarás los principios y conocimientos que debes tener de JavaScript
+
+De la siguiente manera:
+
+ - 01 = consola básica
+
+Contexto: Ejercicios sencillas para aprender como se ejecuta un codigo desde la consola.
